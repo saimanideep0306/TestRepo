9174c0bd191131052645c9329f9221f7db204016
Sai manideep <sai.manideep@verinite.com>
2021-09-17 21:51:39 +0530

    Java Assesment Files

---
+++ Main.java
@@ -0,0 +1,37 @@
+package Manideep;
+
+public class Main{
+	    public int no_of_wheels;
+	    public int no_of_seats;
+	    public void setCar(int No_of_wheels,int no_of_seats)
+	    {
+	        this.no_of_wheels=No_of_wheels;
+	        this.no_of_seats=no_of_seats;
+	    }
+	    public void setBike(int No_of_wheels,int No_of_seats)
+	    {
+	        this.no_of_seats=No_of_seats;
+	        this.no_of_wheels=No_of_wheels;
+	    }
+	    public void getCar()
+	    {
+	                               System.out.println("No_of_wheels_for_car:---"+no_of_wheels);
+			System.out.println("No_of_seat_for_car:---"+no_of_seats);
+	    }
+	    public void getBike()
+	    {
+	                                System.out.println("No_of_wheels_for_bike:---"+no_of_wheels);
+			System.out.println("No_of_seats_for_bike:---"+no_of_seats);
+	    }
+		public static void main(String[] args) {
+			Main c=new Main();
+			Main b=new Main();
+			c.setCar(4,16);
+			b.setBike(2,4);
+			c.getCar();
+			b.getBike();
+			
+		}
+	}
+
+	
---
+++ Main2.java
@@ -0,0 +1,29 @@
+package Manideep;
+public class Main2
+{
+    public String check_leap(int year)
+    {
+        if(year%4==0  && year%100!=0)
+        {
+            return ("yes");
+        }
+    
+        else if (year%400==0)
+        {
+            return("yes");
+        }
+        return ("no");
+    }
+    public static void main(String[] args) {
+		Main2 year=new Main2();
+		int y=100;
+		String check=year.check_leap(y);
+		if(check=="yes")
+		{
+		    System.out.println(y+"---Is a leap year");
+		}
+		else{
+		    System.out.println(y+"---not a leap year");
+		}
+}
+}
